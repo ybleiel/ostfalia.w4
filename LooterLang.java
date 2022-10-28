d8581b48859427a76d72a301aadd6990fd616672
yannis bleiel <yannis.bleiel@gmail.com>
2022-10-28 18:15:17 +0200

    LooterLang - have to check in linux if it passes the tests

---
+++ LooterLang.java
@@ -0,0 +1,59 @@
+import java.lang.constant.Constable;
+
+public class LooterLang
+{
+    public static boolean isConso(char c)
+    {
+        if ((c >= 66 && c <= 68) || (c >= 70 && c <= 72) || (c >= 74 && c <= 78) || (c >= 80 && c <= 84)
+             || (c >= 86 && c <= 90) || (c >= 98 && c <= 100) || (c >= 102 && c <= 104) || (c >= 106 && c <= 110)
+                || (c >= 112 && c <= 116) || (c >= 118 && c <= 122))
+            return (true);
+        else
+            return (false);
+
+    }
+    public static String encode(String s)
+    {
+        int i, l;
+        String lang;
+
+        i = 0;
+        l = s.length();
+        lang = "";
+        while (i < l)
+        {
+            if (isConso(s.charAt(i)) == true)
+            {
+                lang = lang + s.charAt(i) + 'o' + s.charAt(i);
+            }
+            else
+                lang = lang + s.charAt(i);
+            i++;
+        }
+        return lang;
+    }
+
+    public static String decode(String s)
+    {
+        int i, l;
+        String loot;
+
+        i = 0;
+        l = s.length();
+        loot = "";
+        while (i < l)
+        {
+            loot = loot + s.charAt(i);
+            if (isConso(s.charAt(i)) == true)
+                i += 3;
+            else
+                i++;
+        }
+        return loot;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(encode("hallo mein name ist yannis!"));
+    }
+}
